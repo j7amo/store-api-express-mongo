5934a43e21afe6ecfdd1ccb6328eaa9ba9f3cea9
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-23 13:32:07 +0300

    add more dynamic filter products query validation

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -17,15 +17,32 @@ const getAllProducts = async (req, res) => {
   // then the result of the query will be an empty array because there's no documents
   // in DB with this property
   // const products = await Product.find(req.query);
-  const { featured } = req.query;
+  const { featured, company, name } = req.query;
   // the better approach is to:
   // 1) define an object where we'll store the CORRECT (validated) query data
   // 2) check for the properties we expect to be in the query
   // 3) if they are present in the original request - add them to "queryObject"
   const queryObject = {};
 
+  // we check for values in the query params and add them if they are truthy
   if (featured) {
     queryObject.featured = featured === 'true';
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    // here we are not just adding "name" value as it is BUT
+    // we are setting up the Regex object where:
+    queryObject.name = {
+      // - $regex - a pattern to match (e.g. in our case we basically check
+      // if "name" includes the value from the request)
+      // - $options - options (e.g. in our case we use "i" which is for case-insensitive match)
+      $regex: name,
+      $options: 'i',
+    };
   }
 
   const products = await Product.find(queryObject);
