76ea268f9a395b7e32b1a68494dc230a6487ac9d
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-23 12:58:08 +0300

    add basic dynamic filter products query validation

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -11,7 +11,24 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
   // now we set up super basic DYNAMIC filtering.
-  const products = await Product.find(req.query);
+  // when we use straightforward approach of passing req.query DIRECTLY
+  // to Mongoose query methods, we could face the problem:
+  // if we have some unknown properties in the request (in the query parameters)
+  // then the result of the query will be an empty array because there's no documents
+  // in DB with this property
+  // const products = await Product.find(req.query);
+  const { featured } = req.query;
+  // the better approach is to:
+  // 1) define an object where we'll store the CORRECT (validated) query data
+  // 2) check for the properties we expect to be in the query
+  // 3) if they are present in the original request - add them to "queryObject"
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === 'true';
+  }
+
+  const products = await Product.find(queryObject);
   res.status(200).json({ products, nbHits: products.length });
 };
 
