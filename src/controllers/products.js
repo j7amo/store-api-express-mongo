26bbd0db69e801607697c95c6d77e7ed0b1144e9
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-23 12:15:37 +0300

    add static filter products functionality

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,5 +1,12 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: 'products testing route' });
+  // some basic (but static) filter functionality
+  const products = await Product.find({
+    // featured: true,
+    name: 'vase table',
+  });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
